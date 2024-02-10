77eaeda99bcae9f18a80ba56f14a6415dd63232b
Jason Hirata <abstractalchemist@gmail.com>
2024-02-09 17:51:00 -1000

    fixing dropdown

--- src/form/components/dropDownBox/DropDown.jsx
+++ src/form/components/dropDownBox/DropDown.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 import { PropTypes } from 'prop-types';
 
-class DropDown extends React.Component{
-//    constructor(props){
-//        super(props);
-//        this.dropDownChange = this.dropDownChange.bind(this);
-//    }
+class DropDown extends React.Component {
+    //    constructor(props){
+    //        super(props);
+    //        this.dropDownChange = this.dropDownChange.bind(this);
+    //    }
 
-//    dropDownChange(event){
-//        this.props.changeDropDownState(event,this.props.index);
-//    }
+    //    dropDownChange(event){
+    //        this.props.changeDropDownState(event,this.props.index);
+    //    }
+    render() {
+        let choices = [...this.props.choices]
+        choices = [''].concat(choices)
 
-    render(){
         return (
             <div>
                 <label>{this.props.title}</label>
                 <select onChange={(event) => {
                     this.props.changeDropDownState(event, this.props.index);
                 }} value={this.props.value}>
-                    {this.props.choices.map((choice) => <option key={choice}>{choice}</option>)}
+                    {choices.map((choice) => <option key={choice}>{choice}</option>)}
                 </select>
             </div>
         )
