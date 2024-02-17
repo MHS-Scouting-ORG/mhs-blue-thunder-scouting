410cb7359f46998502ecd98a5cfa3f5eca1a7612
dylandomingo57 <dylandomingo57@gmail.com>
2024-02-16 17:52:02 -1000

    refactored Endgame from a class to a function

--- src/form/components/endGameBox/EndGame.jsx
+++ src/form/components/endGameBox/EndGame.jsx
@@ -1,40 +1,24 @@
 import React from "react";
+import { PropTypes } from 'prop-types';
 
-class EndGame extends React.Component {
-    constructor(props) {
-        super(props)
-        this.changeEndGame = this.changeEndGame.bind(this);
-        this.makeEndGameMisc = this.makeEndGameMisc.bind(this);
-        this.state = {
-            changeEndGameUsed: props.value,
-        }
-    }
+function Endgame(props) {
 
-    changeEndGame(event) {
-        this.props.changeEndGameUsed(event);
-        this.setState({ changeEndGameUsed: event.value })
-    }
-
-    makeEndGameMisc() {
-        return (
-            this.props.makeEndGameMisc(this.state.changeEndGameUsed)
-        )
-    }
-
-    render() {
-        return (
-            <div>
-                <label> {"⛓️End Game: "}
-                    <select onChange={this.changeEndGame}>
-                        <option value={this.props.value}> {this.props.value} </option>
-                        <option value='Onstage'>Onstage</option>
-                        <option value='Attempted'>Attempted</option>
-                        <option value='None'>None</option>
-                    </select>
-                </label>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <label> {"⛓️End Game: "}
+                <select onChange={e => props.changeEndGameUsed(e)}>
+                    <option value={props.value}> {props.value} </option>
+                    <option value='Onstage'>Onstage</option>
+                    <option value='Attempted'>Attempted</option>
+                    <option value='None'>None</option>
+                </select>
+            </label>
+        </div>
+    )
 }
 
-export default EndGame;
+Endgame.propTypes = {
+    value: PropTypes.String
+}
+
+export default Endgame
