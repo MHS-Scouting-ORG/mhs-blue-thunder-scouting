f66da31c96684fafab023f296da702f1ab2d98e9
dominickGuiwa <dylandomingo57@gmail.com>
2024-02-22 17:55:15 -1000

    commit for ba to sync

--- src/components/Table/MainTableParts/MTUtils.jsx
+++ src/components/Table/MainTableParts/MTUtils.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { getMatchesForRegional} from "../../../api";
 import { getTeamsInRegional, } from "../../../api/bluealliance";
-import { arrMode, getMax, calcDeviation, calcLowCubeAcc, calcLowCubeGrid, calcLowConeAcc, calcLowConeGrid, calcLowAcc, calcLowGrid, calcMidCubeAcc, calcMidCubeGrid, calcMidConeAcc, calcMidConeGrid, calcMidGridAcc, calcMidGrid, calcUpperCubeAcc, calcUpperCubeGrid, calcUpperConeAcc, calcUpperConeGrid, calcUpperGridAcc, calcUpperGrid, calcAvgCS, calcAvgCubeAcc, calcAvgCubePts, calcAvgConeAcc, calcAvgConePts, calcAvgGrid, calcAvgPoints, getPenalties, getPriorities } from "./CalculationUtils"
+import { } from "./CalculationUtils"
 
 async function getTeams (regional) {
   try {
@@ -95,67 +95,67 @@ async function getTeamsMatchesAndTableData(teamNumbers, oprList, ccwmList, dprLi
       //console.log(teamStats)
       //console.log(teamMatchData)
 
-      const points = teamStats.map(x => x.Teleop.ScoringTotal.Total) //for deviation
-      const gridPoints = teamStats.map(x => x.Teleop.ScoringTotal.GridPoints)
-      const conePts = teamStats.map(x => x.Teleop.ScoringTotal.Cones)
-      const cubePts = teamStats.map(x => x.Teleop.ScoringTotal.Cubes)
-      const coneAcc = teamStats.map(x => x.Teleop.ConesAccuracy.Overall)
-      const cubeAcc = teamStats.map(x => x.Teleop.CubesAccuracy.Overall)
+      // const points = teamStats.map(x => x.Teleop.ScoringTotal.Total) //for deviation
+      // const gridPoints = teamStats.map(x => x.Teleop.ScoringTotal.GridPoints)
+      // const conePts = teamStats.map(x => x.Teleop.ScoringTotal.Cones)
+      // const cubePts = teamStats.map(x => x.Teleop.ScoringTotal.Cubes)
+      // const coneAcc = teamStats.map(x => x.Teleop.ConesAccuracy.Overall)
+      // const cubeAcc = teamStats.map(x => x.Teleop.CubesAccuracy.Overall)
 
-      const mGridPoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgGridPoints.substring(2,8))
-      const mConePoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgConePts.substring(2,8))
-      const mConeAcc = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgConeAcc.substring(2,8)) // for sorts
-      const mCubePoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCubePts.substring(2,8))
-      const mCubeAcc = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCubeAcc.substring(2,8))
-      const mCSPoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCSPoints)
+      // const mGridPoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgGridPoints.substring(2,8))
+      // const mConePoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgConePts.substring(2,8))
+      // const mConeAcc = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgConeAcc.substring(2,8)) // for sorts
+      // const mCubePoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCubePts.substring(2,8))
+      // const mCubeAcc = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCubeAcc.substring(2,8))
+      // const mCSPoints = tableData.filter(x => x.TeamNumber === team.TeamNumber).map(x => x.AvgCSPoints)
       
-      const avgPoints = calcAvgPoints(teamStats)
-      const avgGridPoints = calcAvgGrid(teamStats)
-      const avgConePoints = calcAvgConePts(teamStats)
-      const avgConeAcc = calcAvgConeAcc(teamStats) //tableData
-      const avgCubePoints = calcAvgCubePts(teamStats)
-      const avgCubeAcc = calcAvgCubeAcc(teamStats)
-      const avgCSPoints = calcAvgCS(teamStats)
+      // const avgPoints = calcAvgPoints(teamStats)
+      // const avgGridPoints = calcAvgGrid(teamStats)
+      // const avgConePoints = calcAvgConePts(teamStats)
+      // const avgConeAcc = calcAvgConeAcc(teamStats) //tableData
+      // const avgCubePoints = calcAvgCubePts(teamStats)
+      // const avgCubeAcc = calcAvgCubeAcc(teamStats)
+      // const avgCSPoints = calcAvgCS(teamStats)
 
-      const priorities = getPriorities(teamStats)
-      const penalties = getPenalties(teamStats)
+      // const priorities = getPriorities(teamStats)
+      // const penalties = getPenalties(teamStats)
 
-      const upperGridPts = calcUpperGrid(teamStats)
-      const upperGridAcc = calcUpperGridAcc(teamStats)
-      const midGridPts = calcMidGrid(teamStats)
-      const midGridAcc = calcMidGridAcc(teamStats)
-      const lowerGridPts = calcLowGrid(teamStats)
-      const lowerGridAcc = calcLowAcc(teamStats)
+      // const upperGridPts = calcUpperGrid(teamStats)
+      // const upperGridAcc = calcUpperGridAcc(teamStats)
+      // const midGridPts = calcMidGrid(teamStats)
+      // const midGridAcc = calcMidGridAcc(teamStats)
+      // const lowerGridPts = calcLowGrid(teamStats)
+      // const lowerGridAcc = calcLowAcc(teamStats)
 
-      const upperConeAcc = calcUpperConeAcc(teamStats)
-      const midConeAcc = calcMidConeAcc(teamStats)
-      const lowerConeAcc = calcLowConeAcc(teamStats)
+      // const upperConeAcc = calcUpperConeAcc(teamStats)
+      // const midConeAcc = calcMidConeAcc(teamStats)
+      // const lowerConeAcc = calcLowConeAcc(teamStats)
 
-      const upperConePts = calcUpperConeGrid(teamStats)
-      const midConePts = calcMidConeGrid(teamStats)
-      const lowerConePts = calcLowConeGrid(teamStats)
+      // const upperConePts = calcUpperConeGrid(teamStats)
+      // const midConePts = calcMidConeGrid(teamStats)
+      // const lowerConePts = calcLowConeGrid(teamStats)
 
-      const upperCubeAcc = calcUpperCubeAcc(teamStats)
-      const midCubeAcc = calcMidCubeAcc(teamStats)
-      const lowerCubeAcc = calcLowCubeAcc(teamStats)
+      // const upperCubeAcc = calcUpperCubeAcc(teamStats)
+      // const midCubeAcc = calcMidCubeAcc(teamStats)
+      // const lowerCubeAcc = calcLowCubeAcc(teamStats)
 
-      const upperCubePts = calcUpperCubeGrid(teamStats)
-      const midCubePts = calcMidCubeGrid(teamStats)
-      const lowerCubePts = calcLowCubeGrid(teamStats)
+      // const upperCubePts = calcUpperCubeGrid(teamStats)
+      // const midCubePts = calcMidCubeGrid(teamStats)
+      // const lowerCubePts = calcLowCubeGrid(teamStats)
 
-      const maxGridPoints = getMax(tableData.map(team => team.AvgGridPoints.substring(2,8)))
-      const maxConePoints = getMax(tableData.map(team => team.AvgConePts.substring(2,8)))
-      const maxConeAcc = getMax(tableData.map(team => team.AvgConeAcc.substring(2,8))) //for sorts
-      const maxCubePoints = getMax(tableData.map(team => team.AvgCubePts.substring(2,8)))
-      const maxCubeAcc = getMax(tableData.map(team => team.AvgCubeAcc.substring(2,8)))
-      const maxCSPoints = getMax(tableData.map(team => team.AvgCSPoints))
+      // const maxGridPoints = getMax(tableData.map(team => team.AvgGridPoints.substring(2,8)))
+      // const maxConePoints = getMax(tableData.map(team => team.AvgConePts.substring(2,8)))
+      // const maxConeAcc = getMax(tableData.map(team => team.AvgConeAcc.substring(2,8))) //for sorts
+      // const maxCubePoints = getMax(tableData.map(team => team.AvgCubePts.substring(2,8)))
+      // const maxCubeAcc = getMax(tableData.map(team => team.AvgCubeAcc.substring(2,8)))
+      // const maxCSPoints = getMax(tableData.map(team => team.AvgCSPoints))
 
-      const rGridPoints = mGridPoints / maxGridPoints
-      const rConePoints = mConePoints / maxConePoints
-      const rConeAcc = mConeAcc / maxConeAcc //for sorts
-      const rCubePoints = mCubePoints / maxCubePoints
-      const rCubeAcc = mCubeAcc / maxCubeAcc
-      const rCSPoints = mCSPoints / maxCSPoints
+      // const rGridPoints = mGridPoints / maxGridPoints
+      // const rConePoints = mConePoints / maxConePoints
+      // const rConeAcc = mConeAcc / maxConeAcc //for sorts
+      // const rCubePoints = mCubePoints / maxCubePoints
+      // const rCubeAcc = mCubeAcc / maxCubeAcc
+      // const rCSPoints = mCSPoints / maxCSPoints
 
       //Robot Performance
       const mcRobotSpeed = getMCRobotSpeed(teamStats)
@@ -171,19 +171,25 @@ async function getTeamsMatchesAndTableData(teamNumbers, oprList, ccwmList, dprLi
       const avgSpeaker = calcAvgSpeaker(teamStats)
       const avgAmp = calcAvgAmp(teamStats)
       //robot capabilities 
-      const mcDefend = getMCDefend(teamStats)
+      const mcDefend = arrMode(teamStats.map(val => val.Info.Defend !== null ? val.Info.Defend : 0))
       //custom robot capabilities
-      const mcUnderStage = getMCUnderStage(teamStats)
+      const mcUnderStage = arrMode(teamStats.map(val => val.Info.UnderStage !== null ? val.Info.UnderStage : 0))
       //internal calc
-      const canHang = getHang(teamStats)
+      const canHang = getCan(teamStats.map((x) => x.val !== null ? x.val : 0))
       const canSpeaker = getSpeaker(teamStats)
       const canAmp = getAmp(teamStats)
       const canTrap = getTrap(teamStats)
       //Auto
       const autoStart = getAutoStart(teamStats)
-      const mcScoring = getmcScoring(teamStats)
+      const mcScoring = getMCScoring(teamStats)
       //field info
-      
+      const mcStagePosition = getMCStagePosition(teamStats)
+      //penalties
+      const fouls = getFouls(teamStats)
+      const techs = getTechs(teamStats)
+      const yellowCards = getYellowCards(teamStats)
+      const redCards = getRedCards(teamStats)
+      const brokenRobots = getBrokenRobots(teamStats)
     
       console.log(mcRobotSpeed)
 
@@ -193,47 +199,47 @@ async function getTeamsMatchesAndTableData(teamNumbers, oprList, ccwmList, dprLi
         TeamNumber: team.TeamNumber,
         Matches: team.Matches,
         OPR: oprList[team.TeamNum] ? (oprList[team.TeamNum]).toFixed(2) : null,
-        Priorities: priorities.join(', '),
-        CCWM: ccwmList[team.TeamNum] ? (ccwmList[team.TeamNum]).toFixed(2) : null, 
-        AvgPoints: avgPoints !== 0 && isNaN(avgPoints) !== true ? `μ=${avgPoints}, σ=${calcDeviation(points, avgPoints)}` : '', 
-        AvgGridPoints: avgGridPoints !== 0 && isNaN(avgGridPoints) !== true ? `μ=${avgGridPoints}, σ=${calcDeviation(gridPoints, avgGridPoints)}` : '',
-        AvgCSPoints: avgCSPoints !== 0 && isNaN(avgCSPoints) !== true ? avgCSPoints : '',
-        AvgConePts: avgConePoints !== 0 && isNaN(avgConePoints) !== true ? `μ=${avgConePoints}, σ=${calcDeviation(conePts, avgConePoints)}` : '', 
-        AvgConeAcc: avgConeAcc !== 0 && isNaN(avgConeAcc) !== true ? `μ=${avgConeAcc}, σ=${calcDeviation(coneAcc, avgConeAcc)}` : '', 
-        AvgCubePts: avgCubePoints !== 0 && isNaN(avgCubePoints) !== true ? `μ=${avgCubePoints}, σ=${calcDeviation(cubePts, avgCubePoints)}` : '', 
-        AvgCubeAcc: avgCubeAcc !== 0 && isNaN(avgCubeAcc) !== true ? `μ=${avgCubeAcc}, σ=${calcDeviation(cubeAcc, avgCubeAcc)}` : '', 
-        DPR: dprList[team.TeamNum] ? (dprList[team.TeamNum]).toFixed(2) : null, 
-        Penalties: penalties.join(', '),
+        // Priorities: priorities.join(', '),
+        // CCWM: ccwmList[team.TeamNum] ? (ccwmList[team.TeamNum]).toFixed(2) : null, 
+        // AvgPoints: avgPoints !== 0 && isNaN(avgPoints) !== true ? `μ=${avgPoints}, σ=${calcDeviation(points, avgPoints)}` : '', 
+        // AvgGridPoints: avgGridPoints !== 0 && isNaN(avgGridPoints) !== true ? `μ=${avgGridPoints}, σ=${calcDeviation(gridPoints, avgGridPoints)}` : '',
+        // AvgCSPoints: avgCSPoints !== 0 && isNaN(avgCSPoints) !== true ? avgCSPoints : '',
+        // AvgConePts: avgConePoints !== 0 && isNaN(avgConePoints) !== true ? `μ=${avgConePoints}, σ=${calcDeviation(conePts, avgConePoints)}` : '', 
+        // AvgConeAcc: avgConeAcc !== 0 && isNaN(avgConeAcc) !== true ? `μ=${avgConeAcc}, σ=${calcDeviation(coneAcc, avgConeAcc)}` : '', 
+        // AvgCubePts: avgCubePoints !== 0 && isNaN(avgCubePoints) !== true ? `μ=${avgCubePoints}, σ=${calcDeviation(cubePts, avgCubePoints)}` : '', 
+        // AvgCubeAcc: avgCubeAcc !== 0 && isNaN(avgCubeAcc) !== true ? `μ=${avgCubeAcc}, σ=${calcDeviation(cubeAcc, avgCubeAcc)}` : '', 
+        //DPR: dprList[team.TeamNum] ? (dprList[team.TeamNum]).toFixed(2) : null, 
+        //Penalties: penalties.join(', '),
 
-        AvgUpper: upperGridPts,
-        AvgUpperAcc: upperGridAcc,
-        AvgMid: midGridPts, //for inner tables
-        AvgMidAcc: midGridAcc,
-        AvgLower: lowerGridPts,
-        AvgLowerAcc: lowerGridAcc,
+        // AvgUpper: upperGridPts,
+        // AvgUpperAcc: upperGridAcc,
+        // AvgMid: midGridPts, //for inner tables
+        // AvgMidAcc: midGridAcc,
+        // AvgLower: lowerGridPts,
+        // AvgLowerAcc: lowerGridAcc,
 
-        AvgUpperConeAcc: upperConeAcc,
-        AvgMidConeAcc: midConeAcc,
-        AvgLowerConeAcc: lowerConeAcc,
+        // AvgUpperConeAcc: upperConeAcc,
+        // AvgMidConeAcc: midConeAcc,
+        // AvgLowerConeAcc: lowerConeAcc,
 
-        AvgUpperConePts: upperConePts,
-        AvgMidConePts: midConePts,
-        AvgLowerConePts: lowerConePts,
+        // AvgUpperConePts: upperConePts,
+        // AvgMidConePts: midConePts,
+        // AvgLowerConePts: lowerConePts,
 
-        AvgUpperCubeAcc: upperCubeAcc,
-        AvgMidCubeAcc: midCubeAcc,
-        AvgLowerCubeAcc: lowerCubeAcc,
+        // AvgUpperCubeAcc: upperCubeAcc,
+        // AvgMidCubeAcc: midCubeAcc,
+        // AvgLowerCubeAcc: lowerCubeAcc,
 
-        AvgUpperCubePts: upperCubePts,
-        AvgMidCubePts: midCubePts,
-        AvgLowerCubePts: lowerCubePts,
+        // AvgUpperCubePts: upperCubePts,
+        // AvgMidCubePts: midCubePts,
+        // AvgLowerCubePts: lowerCubePts,
 
-        NGridPoints: isNaN(rGridPoints) !== true ? rGridPoints : 0,
-        NConePoints: isNaN(rConePoints) !== true ? rConePoints : 0, 
-        NConeAccuracy: isNaN(rConeAcc) !== true ? rConeAcc : 0, //for sorts
-        NCubePoints: isNaN(rCubePoints) !== true ? rCubePoints : 0, 
-        NCubeAccuracy: isNaN(rCubeAcc) !== true ? rCubeAcc : 0,
-        NChargeStation: isNaN(rCSPoints) !== true ? rCSPoints : 0,
+        // NGridPoints: isNaN(rGridPoints) !== true ? rGridPoints : 0,
+        // NConePoints: isNaN(rConePoints) !== true ? rConePoints : 0, 
+        // NConeAccuracy: isNaN(rConeAcc) !== true ? rConeAcc : 0, //for sorts
+        // NCubePoints: isNaN(rCubePoints) !== true ? rCubePoints : 0, 
+        // NCubeAccuracy: isNaN(rCubeAcc) !== true ? rCubeAcc : 0,
+        // NChargeStation: isNaN(rCSPoints) !== true ? rCSPoints : 0,
 
         //accessing same/initalized data for now//
 
