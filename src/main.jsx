b79bc7d0d31300bc7b8f8fc5b2e90f5c9dbf4de2
dylandomingo57 <dylandomingo57@gmail.com>
2024-03-04 14:44:25 -1000

    experimenting with ssm parameters

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import App from './App.jsx'
 import './index.css'
 import MainTable from './components/MainTable.jsx'
 import Form from './form/Form.jsx'
-const regional = "2023azva"
+
+const ssm = require('aws-cdk-lib/aws-ssm')
+const regional = ssm.StringParameter.valueForStringParameter(this, 'regionalKey')
+console.log(regional)
 const router = createBrowserRouter([
   {
     path: "/",
