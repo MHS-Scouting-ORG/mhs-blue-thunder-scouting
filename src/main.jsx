f8bfeacf3be4ee66d1318c4f88e27bf8c99cc8b5
dylandomingo57 <dylandomingo57@gmail.com>
2024-03-06 15:46:13 -1000

    experiencing difficulties with login button not loading in on incognito

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,19 @@ import App from './App.jsx'
 import './index.css'
 import MainTable from './components/MainTable.jsx'
 import Form from './form/Form.jsx'
+// import * as Auth from 'aws-amplify/auth'
+// import { GetParameterCommand, SSMClient } from "@aws-sdk/client-ssm"
 import { apiGetRegional } from './api/index.js'
+
+// const apiGetRegional = async function () {
+//   const { credentials } = await Auth.fetchAuthSession()
+//   const client = new SSMClient({ region: 'us-west-1', credentials: credentials })
+//   const command =  new GetParameterCommand({
+//       Name: "regionalKey"
+//   })
+//   const response = await client.send(command)
+//   return response.Parameter.Value
+// }
 
 const regional = await apiGetRegional()
 console.log(regional)
