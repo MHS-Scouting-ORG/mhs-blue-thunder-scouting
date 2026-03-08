f036b5341b54bdaee1631d9078c5d1e6b09f3740
Jason Hirata <abstractalchemist@gmail.com>
2026-03-07 14:53:11 -1000

    logging redirect uri

--- src/amplifyConfig.js
+++ src/amplifyConfig.js
@@ -1,7 +1,11 @@
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports.js";
 
+console.log(`imported from ${import.meta.env.VITE_REDIRECT_INDEX} index`)
+console.log(`redirecting to ${awsExports.oauth.redirectSignIn}`)
+
 const redirectSignInUri = awsExports.oauth.redirectSignIn.split(',')
+console.log(`redirecting to ${awsExports.oauth.redirectSignIn}`)
 awsExports.oauth.redirectSignIn = redirectSignInUri[parseInt(import.meta.env.VITE_REDIRECT_INDEX)]
 const redirectSignOutUri = awsExports.oauth.redirectSignOut.split(',')
 awsExports.oauth.redirectSignOut = redirectSignOutUri[parseInt(import.meta.env.VITE_REDIRECT_INDEX)]
